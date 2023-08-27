d24b7b12bf14e704e4bb548ceaf1bddc01471113
Terross <dima38091@gmail.com>
2023-08-28 00:26:06 +0300

    Add observability

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM gradle:8.0.2-jdk17 AS build
+COPY --chown=gradle:gradle . /src
+WORKDIR /src
+ARG USERNAME
+ARG TOKEN
+
+RUN USERNAME=$USERNAME TOKEN=$TOKEN gradle build --no-daemon
+
+FROM openjdk:17
+
+EXPOSE 6567
+EXPOSE 8082
+
+RUN mkdir /app
+
+COPY --from=build /src/build/libs/wise-task-profile-1.0.0.jar /app/app.jar
+
+ENTRYPOINT ["java","-jar","/app/app.jar"]
--- src/main/java/ru/leti/wise/task/profile/repository/ProfileRepository.java
+++ src/main/java/ru/leti/wise/task/profile/repository/ProfileRepository.java
@@ -1,7 +1,6 @@
 package ru.leti.wise.task.profile.repository;
 
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
+import io.micrometer.observation.annotation.Observed;
 import org.springframework.data.repository.CrudRepository;
 import ru.leti.wise.task.profile.model.ProfileEntity;
 
@@ -9,23 +8,11 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Observed
 public interface ProfileRepository extends CrudRepository<ProfileEntity, UUID> {
 
     Optional<ProfileEntity> findByEmail(String email);
 
     List<ProfileEntity> findAll();
 
-    @Modifying
-    @Query(value = """
-            UPDATE wise_task_profile.profile p
-            SET email = :#{#profile.email},
-                first_name = :#{#profile.firstName},
-                last_name = :#{#profile.lastName},
-                patronymic = :#{#profile.patronymic},
-                student_group = :#{#profile.studentGroup},
-                student_course = :#{#profile.studentCourse},
-                profile_role = :#{#profile.profileRole.toString()}
-            WHERE id = :#{#profile.id}
-            """, nativeQuery = true)
-    int updateProfileBy(ProfileEntity profile);
 }
